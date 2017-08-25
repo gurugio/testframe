23a173fd4d47bef7799ab30942d0eb1bb3908d3e
Gioh Kim <gurugio@hanmail.net>
2017-08-25 23:58:53 +0200

    reading testcase dir

--- a.py
+++ a.py
@@ -1,14 +1,32 @@
 import os
+import sys
+import re
 
 ## environment
-TEST_DIR="./testcase"
+testdir="./testcase"
+total=0
+success=0
+fail=0
 
 print("Start!")
 
 # TEST_DIR exists
-print(os.path.isdir(TEST_DIR))
+if os.path.isdir(testdir):
+    pass
+elif os.access(testdir, R_OK):
+    pass
+else:
+    print("There is not %s directory" % (testdir))
+    sys.exit(1)
 
 # run each scripts
+#test_cases = [f for f in os.listdir(testdir) if re.match(r'*.py', f)]
+test_list = [f for f in os.listdir(testdir)]
+print("test-cases", test_list)
+
+if re.match(r'*.py', "a.py"): # BUGBUG: Why does this not work?
+    print("match")
+
 
 # terminate
-
+print("Finish!: total-test:%d pass:%d fail:%d" % (total, success, fail))
